a570d5c426020ee67c23d2194781411d2db5ac5a
monotraine <syamsudin@eklankuteams.com>
2020-10-20 04:06:01 +0700

    start register bloc bloc

--- lib/modules/modules.dart
+++ lib/modules/modules.dart
@@ -2,3 +2,4 @@ export './authentication/authentication.dart';
 export './message/message.dart';
 export './profile/profile.dart';
 export './init/init.dart';
+export './register/register.dart';
---
+++ lib/modules/register/bloc/bloc.dart
@@ -0,0 +1,7 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:alumina/modules/modules.dart';
+
+part './event.dart';
+part './state.dart';
